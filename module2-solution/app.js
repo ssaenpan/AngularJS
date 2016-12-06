f671548a0970495cdc682e604e51deb1bf2eef86
Sergio Saenz <ssaenpan@hotmail.com>
2016-12-06 09:08:38 -0700

    Module2 ver2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,60 +1,73 @@
 (function () { 
 'use strict'; 
 
-var ShoppingListCheckOffService = [
-	{
-		name: "Cookies",
-		quantity: "10"		
-	},
-	{
-		name: "Sugary drinks",
-		quantity: "20"
-	},
-	{
-		name: "Chips",
-		quantity: "8"
-	},
-	{
-		name: "Candies",
-		quantity: "5"
-	},
-	{
-		name: "Peanuts",
-		quantity: "15"
-	},
-	{
-		name: "Donuts",
-		quantity: "30"
-	}
-];
-
-var itemsBought = [
-	{
-		name: "",
-		quantity: ""
-	}
-];
-
 angular.module('ShoppingListCheckOff', [])
 
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-ToBuyController.$inject = ['$scope'];
- function ToBuyController($scope) { 
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+ function ToBuyController(ShoppingListCheckOffService) { 
  	var BoughttoList = this;	
- 	$scope.ShoppingListCheckOffService = ShoppingListCheckOffService;
- 	
- 	$scope.BoughttoList = function (index) {
- 		$scope.ShoppingListCheckOffService.splice(index, 1);		
- 	};
+ 	BoughttoList.itemsToBuy = ShoppingListCheckOffService.buyItem();
 
-AlreadyBoughtController.$inject = ['$scope'];
- function AlreadyBoughtController($scope) { 
- 	var itemsBought = this;
- 	$scope.itemsBought.name = $scope.ShoppingListCheckOffService.name
+ 	var itemAdder = this;
+ 	itemAdder.addItem = function (index) {
+ 		ShoppingListCheckOffService.addItem(index);
+ 	};
+ 	
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+ function AlreadyBoughtController(ShoppingListCheckOffService) { 
+ 	var itemBought = this;
+ 	itemBought.itemsToBought = ShoppingListCheckOffService.boughtItem();
  };
+
+function ShoppingListCheckOffService() {
+  var service = this;
+	var itemsToBuy = [
+		{
+			name: "Cookies",
+			quantity: "10"		
+		},
+		{
+			name: "Sugary drinks",
+			quantity: "20"
+		},
+		{
+			name: "Chips",
+			quantity: "8"
+		},
+		{
+			name: "Candies",
+			quantity: "5"
+		},
+		{
+			name: "Peanuts",
+			quantity: "15"
+		},
+		{
+			name: "Donuts",
+			quantity: "30"
+		}
+	];
+
+var itemsToBought = [];
+
+  service.addItem = function (index) {
+    itemsToBought.push(itemsToBuy[index]);
+    itemsToBuy.splice(index, 1);
+  };
+
+  service.buyItem = function () {
+    return itemsToBuy;
+  };
+
+  service.boughtItem = function () {
+    return itemsToBought;
+  };
 
 }
 
